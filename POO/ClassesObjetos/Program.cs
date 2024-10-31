ec1f93800af4cbdc6313b98e05d91ceb26d3fb58
Kaue Moura <kauehago07@gmail.com>
2024-10-31 16:47:21 -0300

    POO AULA 31/10

---
+++ POO/ClassesObjetos/Program.cs
@@ -0,0 +1,30 @@
+﻿//Importando as classes de dentro da pasta Classes
+
+using System.Net.Http.Headers;
+using ClassesObjetos.Classes;
+
+//Criar o objeto/variavel animal
+Animal cachorro = new Animal();
+
+cachorro.nome = "Kiara";
+cachorro.raca = "Golden";
+cachorro.idade = "6";
+cachorro.cor = "Dourado";
+
+
+Console.WriteLine($"Nome do cachorrinho: {cachorro.nome}");
+Console.WriteLine($"raca do cachorrinho: {cachorro.raca}");
+Console.WriteLine($"idade do cachorrinho: {cachorro.idade}");
+Console.WriteLine($"cor do cachorrinho: {cachorro.cor}");
+
+Animal peixe = new Animal();
+
+peixe.nome = "Anna";
+peixe.raca = "Telescopio";
+peixe.idade = "17";
+peixe.cor = "Preto";
+
+Console.WriteLine($"Nome do cachorrinho: {peixe.nome}");
+Console.WriteLine($"raca do cachorrinho: {peixe.raca}");
+Console.WriteLine($"idade do cachorrinho: {peixe.idade}");
+Console.WriteLine($"cor do cachorrinho: {peixe.cor}");
